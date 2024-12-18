d2934d481f9462579bb42700cd81bd38504ad66e
Janblack07 <jandryjacob20@gmail.com>
2024-12-17 19:50:32 -0500

    Add Model Usuario and Rol

---
+++ Models/Rol.cs
@@ -0,0 +1,8 @@
+﻿namespace API_TIENDA.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿namespace API_TIENDA.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        // Relación con el Rol
+        public int RolId { get; set; }
+        public Rol Rol { get; set; }
+    }
+}
